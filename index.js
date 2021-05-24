ed13f327cf88f5f48f68f9f61b37622304572243
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-05-24 13:07:40 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copyCats = [...cats];
+    copyCats.push(name);
+    return copyCats
+}
+
+function prependCat(name) {
+    const copyCats = [...cats];
+    copyCats.unshift(name);
+    return copyCats
+}
+
+
+function removeLastCat() {
+    const copyCats = [...cats];
+    copyCats.pop();
+    return copyCats
+}
+
+function removeFirstCat() {
+    const copyCats = [...cats];
+    copyCats.shift();
+    return copyCats
+}
